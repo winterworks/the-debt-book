28c54638750a69ae877f970997b48f438ea09b7c
Vojtěch Rychnovský <rychnvoj@fel.cvut.cz>
2018-10-08 15:27:16 +0200

    WIP list of persons

---
+++ DebtBook/ViewModel/MainViewModel.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace DebtBook
+{
+
+    public class MainViewModel
+    {
+
+        public List<int> List;
+ 
+
+        public MainViewModel()
+        {
+            List = new List<int>();
+            List.Add(1);
+            List.Add(2);
+            List.Add(3);
+            List.Add(4);
+        }
+
+    }
+
+}
