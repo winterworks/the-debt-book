a031c5256d08c95e0b6d6a2f5a38d9ac15579c10
pbnather <p.belczak@me.com>
2018-10-21 12:04:37 +0200

    Move INavigationService to ViewModel namespace

---
+++ Debt Book/ViewModel/INavigationService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Debt_Book.ViewModel
+{
+    interface INavigationService
+    {
+
+        void OpenWindow(ViewModel.AbstractViewModel vm);
+        void CloseWindow(Type vm);
+
+    }
+}
